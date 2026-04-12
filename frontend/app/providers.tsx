17a2653824a47e4cef0063db160650494563cbf4
Rudi Fabricio <243763@ids.upchiapas.edu.mx>
2026-04-11 20:14:58 -0600

    proveedores de contexto agregados

---
+++ frontend/app/providers.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { AuthProvider } from '@/context/AuthContext';
+import { ThemeProvider } from '@/context/ThemeContext';
+
+export default function ClientProviders({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider>
+      <AuthProvider>
+        {children}
+      </AuthProvider>
+    </ThemeProvider>
+  );
+}
